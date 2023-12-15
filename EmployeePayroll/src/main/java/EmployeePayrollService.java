7d8ac82ae7ed7959f7378f509596079245e992f9
akshi0409 <akshita.asah@gmail.com>
2023-12-16 01:47:18 +0530

    Added UC 4 : Update using prepared statement

--- EmployeePayroll/src/main/java/EmployeePayrollService.java
+++ EmployeePayroll/src/main/java/EmployeePayrollService.java
@@ -20,11 +20,6 @@ public class EmployeePayrollService {
     }
 
     public static void main(String[] args) {
-        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-        Scanner consoleInputReader = new Scanner(System.in);
-//        employeePayrollService.readEmployeePayrollData(consoleInputReader);
-//        employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
     }
 
 
